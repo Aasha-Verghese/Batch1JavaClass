b8af1c28c5ba2dcb94b74895c21181e7a78ea381
Aasha Verghese M V <aasha.verghese.mail@gmail.com>
2024-02-26 10:04:25 +0530

    char to int and int  to char type casting tested

---
+++ src/com/jthread/basicjava/TypeCasting.java
@@ -0,0 +1,15 @@
+package com.jthread.basicjava;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		int i=67;
+		char c='A';
+		
+		c=(char)i;
+		
+		System.out.println(c);
+		
+	}
+
+}
